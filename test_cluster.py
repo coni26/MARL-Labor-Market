b24daa52a92b9f6398fda25824d6644235f843e7
coni26 <56028648+coni26@users.noreply.github.com>
2021-08-03 16:58:04 +0200

    Add files via upload

---
+++ test_cluster.py
@@ -0,0 +1,56 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import collections
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers 
+import random
+
+
+
+gamma = 0.95
+n = 1000                            
+w_min, w_max = 2, 5
+w_default = np.linspace(w_min, w_max, n+1)  
+
+
+def u(c, σ=1.3):
+    return (c**(1 - σ) - 1) / (1 - σ)
+
+
+l_ = []
+for salary in w_default:
+    l_.append(u(salary)/(1 - gamma))
+    
+l_35 = []
+for i, w in enumerate(w_default):
+    if w<=3.5:
+        l_35.append(l_[i])
+    else:
+        l_35.append(19.2)
+        
+        
+
+
+def create_model():
+    inputs = layers.Input(shape=(1,))
+    
+    layer1 = layers.Dense(16, activation="linear")(inputs)
+    layer2 = layers.Dense(16, activation="sigmoid")(layer1)
+    layer3 = layers.Dense(16, activation="sigmoid")(layer2)
+    
+
+    action = layers.Dense(1, activation="linear")(layer3)
+
+    return keras.Model(inputs=inputs, outputs=action)
+
+
+
+model = create_model()
+optimizer = keras.optimizers.Adam(learning_rate=0.01)
+loss_function = keras.losses.MeanSquaredError()
+model.compile(optimizer, loss_function)
+hist = model.fit(w_default, np.array(l_35),  epochs = 200, verbose = 0)
+
+sys.stdout.write(str(hist.history['loss'][-1]))
